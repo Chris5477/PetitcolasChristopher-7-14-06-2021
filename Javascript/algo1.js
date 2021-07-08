44769c05b198ea92e7a2b7cec84ee7ca042e6631
ChrisP <petitcolaschristoper@gmail.com>
2021-07-08 15:20:51 +0200

    Modification de l'algo sans es6 pour respecter le cdc (en cours ...)

--- Javascript/algo1.js
+++ Javascript/algo1.js
@@ -1,86 +1,163 @@
 import { ELEMENTHTML } from "./constant.js";
-import { addTags } from "./function.js";
-import { recipes } from "./recipe.js";
-
-export const searchIngredient = (e) => {
+import { createElement, setIngredients } from "./function.js";
+import { recipes } from "./recipe.js"
+let recipeFilter = [];
+let idRecipe = []
+export const mainSearch = (e) => {
+  let arrayId = []
   const inputUser = e.target.value.toLowerCase();
-
   
+  if (inputUser.length <= 3) {
+    ELEMENTHTML.containerRecipe.innerHTML = `<p class="no-result">Aucune recette ne correspond à votre critère ... Vous pouvez rechercher tarte au pomme ou poisson par exemple</p>`;
+  }else{
 
-  let recipeFilter = [];
-  if (inputUser.length < 3) {
-    ELEMENTHTML.containerRecipe.innerHTML = `<p class="no-result">Pour commencer la recherche , veuillez saisir au minimum 3 caractères</p>`;
-  }
+    
+    for (let i = 0; i < recipes.length; i++) {
+      let flag = 0;
+      const element = recipes[i];
+    
+      if(element.name.toLowerCase().match(inputUser) || element.description.toLowerCase().match(inputUser)){
+        recipeFilter.push(element)
+        arrayId.push(element.id)
+        flag = 1
+      }
 
-  for (let i = 0; i < recipes.length; i++) {
-    const currentRecipe = recipes[i];
-    if (currentRecipe.name.toLowerCase().match(inputUser)) {
-      recipeFilter = [...recipeFilter, currentRecipe];
-    }
 
-    for (let j = 0; j < currentRecipe.length; j++) {
-      if (currentRecipe.description.toLowerCase().match(inputUser)) {
-        if (!currentRecipe.id.includes(recipeFilter[j].id)) {
-          recipeFilter = [...recipeFilter, currentRecipe];
-        } else {
+      for (let j = 0; j < recipes[i].ingredients.length; j++) {
+        const ingredient = recipes[i].ingredients[j].ingredient.toLowerCase();
+        if(flag === 1){
           continue;
         }
+       if(ingredient.includes(inputUser)){
+         recipeFilter.push(element)
+       }
+        
       }
     }
+  }
+  
+  createElement(recipeFilter)
+  setIngredients(recipeFilter)
 
-    for (let k = 0; k < currentRecipe.ingredients.length; k++) {
-      const element = currentRecipe.ingredients[k].ingredient.toLowerCase();
-      if (element.includes(inputUser)) {
-        recipeFilter = [...recipeFilter, currentRecipe];
-        ELEMENTHTML.list.innerHTML += `<li>${element}</li>`;
-        ELEMENTHTML.list.classList.add("list-ingredient")
-      }
+  ELEMENTHTML.inputIngredient.addEventListener("click", () => {
+    addAllIngredients(recipeFilter);
+  });
+
+  ELEMENTHTML.inputAppliance.addEventListener("click", () => {
+    addAllAppliances(recipeFilter);
+  });
+
+  ELEMENTHTML.inputUstencil.addEventListener("click", () => {
+    addAllUstencil(recipeFilter);
+  });
+};
+
+export const addAllIngredients = (recipeFilter) => {
+  for (let recipe = 0; recipe < recipeFilter.length; recipe++) {
+  for (let food = 0; food < recipeFilter[recipe].ingredients.length; food++) {
+    const ingredient = recipeFilter[recipe].ingredients[food].ingredient.toLowerCase();
+    
+    
+    if (ELEMENTHTML.listFood.innerHTML.includes(`<li class="ing">${ingredient}</li>`)) {
+      continue;
     }
+    ELEMENTHTML.box[0].appendChild(ELEMENTHTML.listFood);
+    ELEMENTHTML.listFood.innerHTML += `<li class="ing">${ingredient}</li>`;
+    ELEMENTHTML.listFood.classList.add("list-ingredient");
+  }
     
   }
-  [...document.querySelectorAll("li")].forEach((food) => food.addEventListener("click", () => addTags(food, 0, "list-ingredient", "blueTag")));
-  showList(0)
+    
 };
 
-export const searchAppliance = (e) => {
-  const inputUser = e.target.value.toLowerCase();
-  for (let i = 0; i < recipes.length; i++) {
-      const appliance = recipes[i].appliance.toLowerCase();
-      if(appliance.includes(inputUser)){
-          ELEMENTHTML.list.innerHTML+=`<li>${appliance}</li>`
-          ELEMENTHTML.list.classList.add("list-appliance")
-      }
-      
+export const addAllAppliances = (recipeFilter) => {
+  for (let recipe = 0; recipe < recipeFilter.length; recipe++) {
+    const appliance = recipeFilter[recipe].appliance.toLowerCase();
+    if (ELEMENTHTML.listItem.innerHTML.includes(`<li class="object">${appliance}</li>`)) {
+      continue;
+    }
+    ELEMENTHTML.box[1].appendChild(ELEMENTHTML.listItem);
+    ELEMENTHTML.listItem.innerHTML += `<li class="object">${appliance}</li>`;
+    ELEMENTHTML.listItem.classList.add("list-appliance");
   }
-  [...document.querySelectorAll("li")].forEach(item => item.addEventListener("click",() => addTags(item, 1, "list-appliance", "greenTag")))
-  showList(1)
-}
-
-export const searchUstencil = (e) => {
-  const inputUser = e.target.value.toLowerCase();
-  for (let i = 0; i < recipes.length; i++) {
-      for (let j = 0; j < recipes[i].ustensils.length; j++) {
-         const ustensil = recipes[i].ustensils[j].toLowerCase();
-          if(ustensil.includes(inputUser)){
-              ELEMENTHTML.list.innerHTML+=`<li>${ustensil}</li>`
-              ELEMENTHTML.list.classList.add("list-ustensils")
-          }
-          
-      }
-      
-  }
-  [...document.querySelectorAll("li")].forEach(item => item.addEventListener("click", () => addTags(item, 2, "list-ustensil", "redTag")))
-  showList(2)
-}
-
-// Fonction qui permet de montrer les ingredients qui correspondent à la recherche
-const showList = (index) => {
-  // Permet de placer la list sur le bonne input
-  ELEMENTHTML.box[index].appendChild(ELEMENTHTML.list);
-  //ajoute une marge a droite pour afficher coorectement la liste
-  ELEMENTHTML.box[index].style.marginRight = "160px";
-  //ajoute une marge en haut pour eviter que la liste passe au dessus des recettes
-  ELEMENTHTML.containerRecipe.style.marginTop = "200px";
-  // ajoute l'animation du logo des inputs
-  ELEMENTHTML.logoArraow[index].classList.add("animLogo");
 };
+
+export const addAllUstencil = (recipeFilter) => {
+  for (let recipe = 0; recipe < recipeFilter.length; recipe++) {
+    for (let item = 0; item < recipeFilter[recipe].ustensils.length; item++) {
+      const ustencil = recipeFilter[recipe].ustensils[item].toLowerCase();
+    
+      if (ELEMENTHTML.listUstencil.innerHTML.includes(`<li class="item">${ustencil}</li>`)) {
+        continue;
+      }
+      ELEMENTHTML.box[2].appendChild(ELEMENTHTML.listUstencil);
+      ELEMENTHTML.listUstencil.innerHTML += `<li class="item">${ustencil}</li>`;
+      ELEMENTHTML.listUstencil.classList.add("list-ustensils");
+    }
+  }
+};
+
+export const searchElement = (e) => {
+  const inputUser = e.target.value.toLowerCase();
+  const elementsLiHtml = [...document.querySelectorAll("li")];
+  for (const li of elementsLiHtml) {
+    if (inputUser.length >= 3 && li.innerHTML !== inputUser && !li.innerHTML.match(inputUser)) {
+      li.style.display = "none";
+    }
+  }
+  elementsLiHtml.forEach((li) => li.addEventListener("click", () => addTag(li)));
+};
+
+const addTag = (element) => {
+  let cssClass = null;
+  if (element.classList.contains("ing")) {
+    cssClass = "blueTag";
+  } else if (element.classList.contains("object")) {
+    cssClass = "greenTag";
+  } else {
+    cssClass = "redTag";
+  }
+
+  ELEMENTHTML.allTags.innerHTML += `<p class="tag ${cssClass}">${element.innerHTML}<span class="far fa-times-circle"></span></p>`;
+  filterByTag(element.innerHTML);
+  const closeLogo = [...document.querySelectorAll(".fa-times-circle")];
+  closeLogo.forEach((logo, index) => logo.addEventListener("click", () => removeTag(index)));
+};
+
+const filterByTag = (tag) => {
+
+
+  const input = tag.toLowerCase();
+  for (let recipe = 0; recipe < recipeFilter.length; recipe++) {
+    let flag = 0
+    const currentRecipe = recipeFilter[recipe]
+    if(currentRecipe.name.toLowerCase().match(input) || currentRecipe.description.toLowerCase().match(input)){
+      recipeFilter.push(currentRecipe)
+      flag = 1
+    }
+    
+    
+    
+    idRecipe = recipeFilter.map((recipeId) => recipeId.id);
+    
+    
+    for (const food of recipe.ingredients) {
+      const ingredient = food.ingredient.toLowerCase();
+      if (flag === 1){
+        continue;
+      }
+      if (ingredient.includes(input)) {
+        recipeFilter = [...recipeFilter, recipe];
+      }
+  }
+}
+  createElement(recipeFilter);
+  setIngredients(recipeFilter);
+};
+
+const removeTag = (index) => {
+  const tag = [...document.querySelectorAll(".tag")][index].remove();
+  ELEMENTHTML.allTags.innerHTML === "" ? createElement(recipes) + setIngredients(recipes) + addAllIngredients(recipes) : filterByTag(tag);
+};
+
+  
--- Javascript/app.js
+++ Javascript/app.js
@@ -1,12 +1,12 @@
-import { ELEMENTHTML } from "./constant.js";
-import { recipes } from "./recipe.js";
-import { createElement, setIngredients } from "./function.js";
-import { searchIngredient, searchAppliance, searchUstencil } from "./algo1.js";
+import { recipes } from "./recipe.js"
+import { ELEMENTHTML } from "./constant.js"
+import { createElement, setIngredients } from "./function.js"
+import { mainSearch, searchElement } from "./algo1.js"
 
-createElement(recipes);
-setIngredients(recipes);
+createElement(recipes)
+setIngredients(recipes)
 
-ELEMENTHTML.mainSearch.addEventListener("change", (e) => searchIngredient(e));
-ELEMENTHTML.inputIngredient.addEventListener("change", (e) => searchIngredient(e));
-ELEMENTHTML.inputAppliance.addEventListener("change", (e) => searchAppliance(e));
-ELEMENTHTML.inputUstencil.addEventListener("change", (e) => searchUstencil(e));
+ELEMENTHTML.mainSearch.addEventListener("input" , (e) => mainSearch(e))
+ELEMENTHTML.inputIngredient.addEventListener("change", (e) => searchElement(e))
+ELEMENTHTML.inputAppliance.addEventListener("change", (e) => searchElement(e))
+ELEMENTHTML.inputUstencil.addEventListener("change", (e) => searchElement(e))
